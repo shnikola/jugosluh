dba8bd58b99df790892d8085d03be602290aa9f4
Nikola Šantić <nikola@readcubes-MacBook-Pro.local>
2016-05-02 19:57:57 +0200

    Player shows all tracks now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,17 @@
 $(function() {
   $('a[data-popup]').on('click', function(e) {
-    window.open($(this).attr('href'), "Play", 'height=550,width=850'); 
+    window.open($(this).attr('href'), "Play", 'height=550,width=850');
     e.preventDefault();
   });
-  
+
   $("input.autosubmit, select.autosubmit").on("change", function() {
     $(this).parents("form").submit();
   });
-  
+
   var audio = $(".player").get(0);
   var tracklist = [];
   var currentTrack = 0;
-   
+
   $(".play-button").on("click", function() {
     if (tracklist.length > 0) {
       play(0);
@@ -27,14 +27,14 @@ $(function() {
     }
     return false;
   });
-  
+
   function initPlayer() {
+    for (var i = 0; i < tracklist.length; i++) {
+      $(".player-container .playlist").append("<div class='track' data-track=" + i + ">" + tracklist[i].title + "</div>")
+    }
     $(".player-container").show();
-    $(".player-container .prev").on("click", function() {
-      play(currentTrack - 1);
-    });
-    $(".player-container .next").on("click", function() {
-      play(currentTrack + 1);
+    $(".player-container .track").on("click", function() {
+      play($(this).data("track"))
     });
     $(audio).on("ended", function() {
       if (currentTrack < tracklist.length - 1) {
@@ -42,12 +42,12 @@ $(function() {
       }
     });
   }
-  
+
   function play(track) {
     currentTrack = (track + tracklist.length) % tracklist.length;
     audio.src = tracklist[currentTrack].url;
     audio.load();
     audio.play();
-    $(".player-container .title").text(tracklist[currentTrack].title)
+    $(".player-container .track").removeClass("active").eq(currentTrack).addClass("active");
   }
 });
